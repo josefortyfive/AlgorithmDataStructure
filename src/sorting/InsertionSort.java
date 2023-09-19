84e4e662714466069dae6c7c4db4cca9e5f06525
Edmar <jose.fortyfive@gmail.com>
2023-09-19 12:36:43 +0800

    First code Insertionsort

---
+++ src/sorting/InsertionSort.java
@@ -0,0 +1,34 @@
+package sorting;
+
+public class InsertionSort {
+
+	public static void insertionSort(int arr[]) {
+		int n = arr.length;
+		
+		for(int i = 0; i < n; i++) {
+			int key = arr[i];
+			int j = i - 1;
+			
+			while(j >= 0 && arr[j] > key) {
+				arr[j + 1] = arr[j];
+				j = j - 1;
+			}
+			arr[j + 1] = key;
+		}
+	}
+	
+	public static void printArray(int arr[]) {
+		int n = arr.length;
+		for(int i = 0; i < n; i++) {
+			System.out.print(" "+arr[i]);
+		}
+	}
+	
+	public static void main(String args[]) {
+		
+		int arr[] = { 1234, 54, 211, 41, 1, 20};
+		
+		insertionSort(arr);
+		printArray(arr);
+	}
+}
