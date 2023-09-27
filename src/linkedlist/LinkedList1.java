380cdb874763bbe3f7c12998738ddac8705160c2
Edmar <jose.fortyfive@gmail.com>
2023-09-27 20:40:03 +0800

    added linkedlist

---
+++ src/linkedlist/LinkedList1.java
@@ -0,0 +1,18 @@
+package linkedlist;
+
+import java.util.LinkedList;
+
+public class LinkedList1 {
+	
+	public static void main(String args[]) {
+		LinkedList<String> animals = new LinkedList<String>();
+		animals.add("Dog");
+		animals.add("Cat");
+		animals.add("Birds");
+		animals.add("Horse");
+		animals.add("Cow");
+		System.out.println(animals);
+	}
+
+
+}
