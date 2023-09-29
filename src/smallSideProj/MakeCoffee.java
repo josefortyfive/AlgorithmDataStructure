0538de3e9137c46ce2024d28c7bb421eec524eb9
Edmar <jose.fortyfive@gmail.com>
2023-09-29 15:14:31 +0800

    MakeCoffee java

---
+++ src/smallSideProj/MakeCoffee.java
@@ -0,0 +1,35 @@
+package smallSideProj;
+
+public class MakeCoffee {
+
+	public static void main(String args[]) {
+		Coff makeCof = Coff.Chocolate;
+		
+		switch(makeCof) {
+		case Coffee: printArray(new String[]{
+				"Add Coffee",
+				"Add Creamer",
+				"Add Sugar",
+				"Mix well"});
+			break;
+		case Chocolate: printArray(new String[] {
+				"Add Chocolate"
+		});
+			break;
+		}
+		
+	}
+	
+	public static void printArray(String arr[]) {
+		for(String i : arr) {
+			System.out.println(" "+i);
+		}
+	}
+
+}
+
+
+enum Coff{
+	Coffee,
+	Chocolate;
+}
