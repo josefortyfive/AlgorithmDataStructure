1d24b5d44cb8a2e2d19599260a558d1d45e347c4
Edmar <jose.fortyfive@gmail.com>
2023-10-10 20:25:52 +0800

    Fibonacci

---
+++ src/recusion/Fibonacci.java
@@ -0,0 +1,34 @@
+package recusion;
+
+import java.util.*;
+
+public class Fibonacci {
+
+	
+	public static int fib(int n) {
+		if(n == 0) {
+			return 0;
+		}
+		if(n == 1 || n == 2) {
+			return 1;
+		}
+		else {
+			return(fib(n - 1) + fib(n - 2));
+		}
+	}
+	
+	public static void main(String args[]) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Enter the number: ");
+		int n = in.nextInt();
+		
+		System.out.println("Fibonacci: ");
+		
+		for(int i = 0; i < n; i++) {
+			System.out.print(fib(i)+ " ");
+			 
+		}
+	}
+}
