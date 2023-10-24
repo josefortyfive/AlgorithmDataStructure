bca3eda30096313550e28057928c242b7c69d127
Edmar <jose.fortyfive@gmail.com>
2023-10-24 23:01:30 +0800

    void

--- src/sorting/CockTailSort.java
+++ src/sorting/CockTailSort.java
@@ -2,7 +2,46 @@ package sorting;
 
 public class CockTailSort {
 
-	public static void 
+	public static void cockTailSort(int arr[]) {
+		boolean swapped = true;
+		int start = 0;
+		int end = arr.length;
+		
+		while(swapped == true) {
+			
+			swapped = false;
+			
+			for(int i = start; i < end - 1; i++) {
+				if(arr[i] > arr[i + 1]) {
+					int temp = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = temp;
+					swapped = true;
+				}
+			}
+			
+			if(swapped == false) {
+				break;
+			}
+			
+			swapped = false;
+			
+			end = end - 1;
+			
+			for(int i = end - 1; i >= start; i--) {
+				if(arr[i] > arr[i + 1]) {
+					int temp = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = temp;
+					swapped = true;
+				}
+			}
+			
+			start = start + 1;
+		}	
+	}
+	
+	
 	
 	
 }
