60dc292b34211508475781195fb0aabba7da1921
Edmar <jose.fortyfive@gmail.com>
2023-10-25 15:31:06 +0800

    added the function for recursion

--- src/recusion/RecursionExample2.java
+++ src/recusion/RecursionExample2.java
@@ -6,7 +6,16 @@ public class RecursionExample2 {
 	static int count = 0;
 	
 	static void recursionSample() {
+		count++;
 		
+		if(count <= 5) {
+			System.out.println("Hello "+count);
+			recursionSample();
+		}
+	}
+	
+	public static void main(String args[]) {
+		recursionSample();
 	}
 	
 }
