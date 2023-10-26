60e026cb1f34de13df888a7da35d925bf81a137b
Edmar <jose.fortyfive@gmail.com>
2023-10-26 10:37:22 +0800

    print Function for Combsort

--- src/sorting/CombSort.java
+++ src/sorting/CombSort.java
@@ -38,4 +38,17 @@ public class CombSort {
 			}	
 		}	
 	}
+	
+	
+	public static void printArray(int arr[]) {
+		int n = arr.length;
+		
+		for(int i = 0; i < n; i++) {
+			System.out.print(" "+arr[i]);
+		}
+	}
+	
+	public static void main(String args[]) {
+		
+	}
 }
