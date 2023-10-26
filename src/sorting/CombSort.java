044bba10f184856a8db503eb5086e2278ac7baa2
Edmar <jose.fortyfive@gmail.com>
2023-10-26 10:35:48 +0800

    add function for comb sort

--- src/sorting/CombSort.java
+++ src/sorting/CombSort.java
@@ -1,5 +1,41 @@
 package sorting;
 
 public class CombSort {
+	
+	public static int getNextGap(int gap) {
+		gap = (gap * 10) / 13;
+		if(gap < 1) { // Checks the value of the input if greater than 1
+			return 1; 
+		}
+		
+		return gap;
+	}
 
+	// Sort function for CombSort
+	public static void combSort(int arr[]) {
+		
+		int n = arr.length; // get the length of the array to loop 
+		
+		
+		int gap = n; // intialize gap
+		
+		
+		boolean swapped = true; // swaps the value if it's true
+		
+		while(gap != 1 || swapped == true) {
+			
+			// Find next gap
+			
+			gap = getNextGap(gap);
+			
+			
+			swapped = false; // Swapped if false to check if it happend or not
+			
+			for(int i = 0; i < n - gap; i++) {
+				int temp = arr[i];
+				arr[i] = arr[i + gap];
+				arr[i + gap] = temp;
+			}	
+		}	
+	}
 }
