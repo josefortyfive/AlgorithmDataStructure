120f5a7677c029ecd5b39163239d486c7f4c0d12
Edmar <jose.fortyfive@gmail.com>
2023-10-29 20:02:55 +0800

    added gnome function

--- src/sorting/GnomeSort.java
+++ src/sorting/GnomeSort.java
@@ -2,8 +2,28 @@ package sorting;
 
 public class GnomeSort {
 	
-	public static void gnomeSort(int arr[]) {
+	public static void gnomeSort(int arr[], int n) {
+		int index = 0;
 		
+		while(index < n) {
+			if(index < n) {
+				index++;
+			}
+			
+			if(arr[index] >= arr[index - 1]) {
+				index++;
+			}
+			else {
+				int temp = 0;
+				temp = arr[index];
+				arr[index] = arr[index - 1];
+				arr[index - 1] = temp;
+				index--;
+			}
+		}
+		
+		
+		return;
 		
 	}
 	
