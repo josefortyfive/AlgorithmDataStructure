7001fea75bf8b21028d08728772136736a31b73f
Edmar <jose.fortyfive@gmail.com>
2023-10-30 20:37:56 +0800

    added function for stoogeSort

--- src/sorting/StoogeSort.java
+++ src/sorting/StoogeSort.java
@@ -1,6 +1,33 @@
 package sorting;
 
 public class StoogeSort {
+	
+	public static void stoogeSort(int arr[], int l, int h) {
+		if( l >= h) {
+			return;
+		}
+		
+		// If first element is smaller than the last, swap them
+		
+		if(arr[l] > arr[h]) {
+			int temp = arr[l];
+			arr[l] = arr[h];
+			arr[h] = temp;
+		}
+		
+		// If there are more than 2 elements in the array
+		
+		if(h - l + 1 > 2) {
+			int temp = (h - l + 1) / 3;
+			
+			stoogeSort(arr, l, h - temp);
+			
+			stoogeSort(arr, l + temp, h);
+			
+			stoogeSort(arr, l, h - temp);
+			
+		}
+	}
 
 	
 	public static void printArray(int arr[]) {
