6f189c966e5ea176b06538da52dba11b871a3c7a
Edmar <jose.fortyfive@gmail.com>
2023-10-31 20:05:07 +0800

    added sort function

--- src/sorting/Pancake.java
+++ src/sorting/Pancake.java
@@ -24,4 +24,37 @@ public class Pancake {
                 mi = i;
         return mi;
     }
+    
+    static int pancakeSort(int arr[], int n)
+    {
+        // Start from the complete
+        // array and one by one
+        // reduce current size by one
+        for (int curr_size = n; curr_size > 1; 
+                                 --curr_size)
+        {
+            // Find index of the
+            // maximum element in
+            // arr[0..curr_size-1]
+            int mi = findMax(arr, curr_size);
+ 
+            // Move the maximum element
+            // to end of current array
+            // if it's not already at 
+            // the end
+            if (mi != curr_size-1)
+            {
+                // To move at the end,
+                // first move maximum
+                // number to beginning
+                flip(arr, mi);
+ 
+                // Now move the maximum 
+                // number to end by
+                // reversing current array
+                flip(arr, curr_size-1);
+            }
+        }
+        return 0;
+    }
 }
