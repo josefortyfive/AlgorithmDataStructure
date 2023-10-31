1312980d6df36072984740d419cd7af1b8041923
Edmar <jose.fortyfive@gmail.com>
2023-10-31 20:03:40 +0800

    added flip function

--- src/sorting/Pancake.java
+++ src/sorting/Pancake.java
@@ -2,4 +2,17 @@ package sorting;
 
 public class Pancake {
 
+	
+    static void flip(int arr[], int i)
+    {
+        int temp, start = 0;
+        while (start < i)
+        {
+            temp = arr[start];
+            arr[start] = arr[i];
+            arr[i] = temp;
+            start++;
+            i--;
+        }
+    }
 }
