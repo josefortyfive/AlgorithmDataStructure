90ea67c39caf648d1779c6d004e5c12dd8842469
Edmar <jose.fortyfive@gmail.com>
2023-11-07 21:04:51 +0800

    added the Constructor

--- src/heap/MinHeap.java
+++ src/heap/MinHeap.java
@@ -1,5 +1,19 @@
 package heap;
 
 public class MinHeap {
+	
+	private int[] heapArray;
+	
+	
+	private int capacity;
+	
+	private int current_heap_size;
+	
+	
+	public MinHeap(int n) {
+		capacity = n;
+		heapArray = new int[capacity];
+		current_heap_size = 0;
+	}
 
 }
