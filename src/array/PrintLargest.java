750ffa7f9982eef25424f6fa57c2153c773a7d5e
Edmar <jose.fortyfive@gmail.com>
2023-11-09 20:40:40 +0800

    added variable for the function

--- src/array/PrintLargest.java
+++ src/array/PrintLargest.java
@@ -2,4 +2,14 @@ package array;
 
 public class PrintLargest {
 
+	public static void print3Largest(int arr[], int arr_size) {
+		int i;
+		int first;
+		int second;
+		int third;
+		
+		if(arr_size < 3) {
+			System.out.print(" Invalid Input ");
+		}
+	}
 }
