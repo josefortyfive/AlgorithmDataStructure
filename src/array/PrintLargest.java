b9ad41555f0cf2934447d6f6f898b7705b2d2198
Edmar <jose.fortyfive@gmail.com>
2023-11-09 20:45:58 +0800

    finish the function

--- src/array/PrintLargest.java
+++ src/array/PrintLargest.java
@@ -3,7 +3,6 @@ package array;
 public class PrintLargest {
 
 	public static void print3Largest(int arr[], int arr_size) {
-		int i;
 		int first;
 		int second;
 		int third;
@@ -11,5 +10,35 @@ public class PrintLargest {
 		if(arr_size < 3) {
 			System.out.print(" Invalid Input ");
 		}
+		
+		
+		first = Integer.MIN_VALUE;
+		second = Integer.MIN_VALUE;
+		third = Integer.MIN_VALUE;
+		
+		
+		for(int i = 0; i < arr_size; i++){
+			
+			if(arr[i] > first){
+				third = second;
+				second = first;
+				first = arr[i];
+			}
+			
+			else if (arr[i] > second){
+				third = second;
+				second = arr[i];
+			}
+			
+			else if(arr[i] > third){
+				third = arr[i];
+			}
+		}
+		
+		System.out.println("The three largest elements are " + first+ " "
+				+second+ " " +third);
 	}
+	
+	
+	
 }
