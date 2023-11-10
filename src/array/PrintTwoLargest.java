8d8a918af077666f0ca3d97a7bd6e2258f8a4970
Edmar <jose.fortyfive@gmail.com>
2023-11-10 22:14:59 +0800

    added the print2largest function

--- src/array/PrintTwoLargest.java
+++ src/array/PrintTwoLargest.java
@@ -1,6 +1,32 @@
 package array;
 
+import java.util.*;
 public class PrintTwoLargest {
 
+	public static void print2Largest(int arr[]) {
+		int first;
+		int second;
+		int n = arr.length;
+		
+		if(n < 2) {
+			System.out.printf("Invalid Input ");
+			return;
+		}
+		
+		Arrays.sort(arr);
+		
+		
+		for(int i = n - 2; i  >= 0; i--) {
+			if(arr[i]  != arr[n - 1]) {
+				System.out.printf("The second largest " + arr[i]);
+				
+				return;
+			}
+		}
+		
+		System.out.printf("No second largest array");
+		
+	}
 	
+
 }
