015d0da3c0f9a729590dab771424ab08a3f3544b
Edmar <jose.fortyfive@gmail.com>
2023-11-14 22:25:50 +0800

    added the pushZeroToEnd function

--- src/array/PushZero.java
+++ src/array/PushZero.java
@@ -2,5 +2,19 @@ package array;
 
 public class PushZero {
 
-	
+	public static void pushZerosToEnd(int arr[]) {
+		
+		int n = arr.length;
+		int count = 0;
+		
+		for(int i = 0; i < n; i++) {
+			if (arr[i] != 0) {
+				arr[count++] = arr[i];
+			}
+		}
+		
+		while(count < n) {
+			arr[count++] = 0;
+		}
+	}
 }
