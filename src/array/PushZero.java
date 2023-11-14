0351086253857c62159da313b0c02b56c290b7f1
Edmar <jose.fortyfive@gmail.com>
2023-11-14 22:26:57 +0800

    added the printArray function in PushZero class

--- src/array/PushZero.java
+++ src/array/PushZero.java
@@ -17,4 +17,14 @@ public class PushZero {
 			arr[count++] = 0;
 		}
 	}
+	
+	public static void printArray(int arr[]) {
+		int n = arr.length;
+		
+		for(int i = 0; i < n; i++) {
+			System.out.print(arr[i]+ " ");
+		}
+	}
+	
+	
 }
