51677b3d414febbd85ac533625fb5367ec2b095d
Edmar <jose.fortyfive@gmail.com>
2023-11-18 08:07:08 +0800

    added the arrayEvenAndOdd function

--- src/array/SegregateOddEvenArray.java
+++ src/array/SegregateOddEvenArray.java
@@ -1,5 +1,24 @@
 package array;
 
 public class SegregateOddEvenArray {
+	
+	static void arrayEvenAndOdd(int arr[]) {
+		int n = arr.length;
+		
+		int i = -1;
+		int j = 0;
+		
+		while (j != n) {
+			if(arr[j] % 2 == 0) {
+				i++;
+			}
+			
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		}
+		
+		j++;
+	}
 
 }
