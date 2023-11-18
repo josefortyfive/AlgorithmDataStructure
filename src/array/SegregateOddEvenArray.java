9d2d0794d1502c4fd88ca9d1432d192e784a30bd
Edmar <jose.fortyfive@gmail.com>
2023-11-18 08:08:06 +0800

    added printArray function

--- src/array/SegregateOddEvenArray.java
+++ src/array/SegregateOddEvenArray.java
@@ -20,5 +20,13 @@ public class SegregateOddEvenArray {
 		
 		j++;
 	}
+	
+	static void printArray(int arr[]) {
+		int n = arr.length;
+		
+		for(int i = 0; i < n; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
 
 }
