a6fe2ee4dc9cb055e2a791aebc0b2c5ae9987eaa
Edmar <jose.fortyfive@gmail.com>
2023-11-19 13:12:14 +0800

    added function reverseArray and added the function in leftRotate function

--- src/array/LeftRotate.java
+++ src/array/LeftRotate.java
@@ -11,5 +11,24 @@ public class LeftRotate {
 		
 		d = d % n;
 		
+		reverseArray(arr, 0, d - 1);
+		reverseArray(arr, d, n - 1);
+		reverseArray(arr, 0, n - 1);
+		
 	}
+	
+	static void reverseArray(int arr[], int start, int end) {
+		int temp;
+		
+		while (start < end) {
+			temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+			start++;
+			end--;
+		}
+	}
+	
+	
+	
 }
