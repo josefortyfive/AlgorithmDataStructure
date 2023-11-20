a2805ed399f8e644d98d71448d83863e59875283
Edmar <jose.fortyfive@gmail.com>
2023-11-20 10:01:19 +0800

    added sortInWave function

--- src/array/SortWave.java
+++ src/array/SortWave.java
@@ -1,5 +1,7 @@
 package array;
 
+import java.util.Arrays;
+
 public class SortWave {
 
 	static void swap(int arr[], int a, int b) {
@@ -8,4 +10,11 @@ public class SortWave {
 		arr[b] = temp;
 	}
 	
+	static void sortInWave(int arr[], int n) {
+		Arrays.sort(arr);
+		
+		for(int i = 0; i < n - 1; i += 2) {
+			swap(arr, i, i + 1);
+		}
+	}
 }
