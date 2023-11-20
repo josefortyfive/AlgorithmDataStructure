2e09856a9dff8bf20d45e88bdad403bc80e261dd
Edmar <jose.fortyfive@gmail.com>
2023-11-20 10:02:49 +0800

    added main function for SortWave

--- src/array/SortWave.java
+++ src/array/SortWave.java
@@ -17,4 +17,14 @@ public class SortWave {
 			swap(arr, i, i + 1);
 		}
 	}
+	
+	public static void main(String args[]) {
+		int arr[] = {10, 90, 49, 2, 1, 5, 23};
+		int n = arr.length;
+		
+		sortInWave(arr, n);
+		for(int i = 0; i < n; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
 }
