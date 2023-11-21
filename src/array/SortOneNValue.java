30aa3091e2ad52c9636b443c862cbd9536168a74
Edmar <jose.fortyfive@gmail.com>
2023-11-21 10:13:48 +0800

    added sort function

--- src/array/SortOneNValue.java
+++ src/array/SortOneNValue.java
@@ -2,4 +2,22 @@ package array;
 
 public class SortOneNValue {
 
+	
+	
+	static void sort(int arr[]) {
+		int i = 0;
+		
+		while ( i < arr.length) {
+			int correct = arr[i] - 1;
+			
+			if(arr[correct] != arr[i]) {
+				swap(arr, i, correct);
+			}
+			
+			else {
+				i++;
+			}
+		}
+	}
+	
 }
