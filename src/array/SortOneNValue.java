118156f9791cd4b565a5455a6db89de7fc4ade87
Edmar <jose.fortyfive@gmail.com>
2023-11-21 10:14:47 +0800

    added swap function

--- src/array/SortOneNValue.java
+++ src/array/SortOneNValue.java
@@ -2,8 +2,6 @@ package array;
 
 public class SortOneNValue {
 
-	
-	
 	static void sort(int arr[]) {
 		int i = 0;
 		
@@ -18,6 +16,12 @@ public class SortOneNValue {
 				i++;
 			}
 		}
+	}
+	
+	static void swap(int arr[], int first, int second) {
+		int temp = arr[first];
+		arr[first] = arr[second];
+		arr[second] = temp;
 	}
 	
 }
