9f66cd45217aedea7095112d1eabba1817b8214b
Edmar <jose.fortyfive@gmail.com>
2023-11-27 16:00:14 +0800

    added findElementFunction

--- src/array/FindArrayElement.java
+++ src/array/FindArrayElement.java
@@ -5,6 +5,24 @@ public class FindArrayElement {
 	
 	static int findSingle(int arr[]) {
 		
+		int sizeOfArray = arr.length;
+		
+		for(int i = 0; i < sizeOfArray; i++) {
+			int count = 0;
+			
+			for(int j = 0; j < sizeOfArray; i++) {
+				
+				if(arr[i] == arr[j]) {
+					count++;
+				}
+			}
+			
+			
+			if(count == 1) {
+				return arr[i];
+			}
+		}
+		
 		
 		return -1;
 	}
