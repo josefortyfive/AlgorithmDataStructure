35bd959fd8824b8314faaa5d0022bb33190863d8
Edmar <jose.fortyfive@gmail.com>
2023-11-27 17:00:26 +0800

    added comment

--- src/array/FindArrayElement.java
+++ src/array/FindArrayElement.java
@@ -8,15 +8,19 @@ public class FindArrayElement {
 		int sizeOfArray = arr.length;
 		
 		for(int i = 0; i < sizeOfArray; i++) {
+			
+			// Initialize count to 0
 			int count = 0;
 			
 			for(int j = 0; j < sizeOfArray; j++) {
 				
+				// Count the frequency of the element
 				if(arr[i] == arr[j]) {
 					count++;
 				}
 			}
 			
+			// if the frequency of the element is one
 			
 			if(count == 1) {
 				return arr[i];
