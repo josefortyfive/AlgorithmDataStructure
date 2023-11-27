dadb7b72a6e6e7b77790d7d54e7182e1474d8bc0
Edmar <jose.fortyfive@gmail.com>
2023-11-27 16:02:52 +0800

    added full function of FindArrayElement

--- src/array/FindArrayElement.java
+++ src/array/FindArrayElement.java
@@ -10,7 +10,7 @@ public class FindArrayElement {
 		for(int i = 0; i < sizeOfArray; i++) {
 			int count = 0;
 			
-			for(int j = 0; j < sizeOfArray; i++) {
+			for(int j = 0; j < sizeOfArray; j++) {
 				
 				if(arr[i] == arr[j]) {
 					count++;
@@ -26,6 +26,13 @@ public class FindArrayElement {
 		
 		return -1;
 	}
+	
+	
+	public static void main(String args[]) {
+		int arr[] = {2, 3, 5, 4, 5, 3, 4};
+		
+		System.out.println("Element occuring once: " +findSingle(arr));
+ 	}
 	
 	
 }
--- src/array/PrintUniqueElement3.java
+++ src/array/PrintUniqueElement3.java
@@ -1,5 +1,5 @@
 package array;
-
+ 
 
 import java.io.*;
 import java .util.*;
