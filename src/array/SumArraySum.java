0ce2cc45dcb11fd16261c0408b175f1e41f23460
Edmar <jose.fortyfive@gmail.com>
2023-12-02 21:54:04 +0800

    added search for the SumArraySum

--- src/array/SumArraySum.java
+++ src/array/SumArraySum.java
@@ -2,4 +2,18 @@ package array;
 
 public class SumArraySum {
 
+	
+	static void subArraySum(int arr[], int sum) {
+		int lengthOfTheArray = arr.length;
+		
+		for(int i = 0; i < lengthOfTheArray; i++) {
+			
+			int currentSum = arr[i];
+			
+			if(currentSum == sum) {
+				System.out.println("Sum found at index " +i);
+			}
+		}
+		
+	}
 }
