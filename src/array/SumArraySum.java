4e3b47d3a9ea947db77a15870759a7b2053a84fa
Edmar <jose.fortyfive@gmail.com>
2023-12-02 21:58:43 +0800

    added the main function for SumArraySum

--- src/array/SumArraySum.java
+++ src/array/SumArraySum.java
@@ -22,13 +22,23 @@ public class SumArraySum {
 					
 					if(currentSum == sum) {
 						System.out.println("Sum found between indexes " + " and " +j);
+
+						return;
 					}
 					
-					return;
 				}
 			}
 		}
 		System.out.println("No subArray found ");
 		return;
+	}
+	
+	public static void main(String args[]) {
+		
+		int arr[] = {15, 2, 4, 8, 9, 5, 10, 23 };
+		int n = arr.length;
+		int sum = 23;
+		subArraySum(arr, sum);
+		
 	}
 }
