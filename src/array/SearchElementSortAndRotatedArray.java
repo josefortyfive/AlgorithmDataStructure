c05da958e72ca01fafe2f0e242846a26a546c3e7
Edmar <jose.fortyfive@gmail.com>
2023-12-10 15:44:18 +0800

    added pivot function

--- src/array/SearchElementSortAndRotatedArray.java
+++ src/array/SearchElementSortAndRotatedArray.java
@@ -25,4 +25,24 @@ public class SearchElementSortAndRotatedArray {
 	         return binarySearch(arr, 0, pivot - 1, key);
 	     return binarySearch(arr, pivot + 1, n - 1, key);
 	 }
+	 
+    static int findPivot(int arr[], int low, int high)
+    {
+        // base cases
+        if (high < low)
+            return -1;
+        if (high == low)
+            return low;
+ 
+        /* low + (high - low)/2; */
+        int mid = (low + high) / 2;
+        if (mid < high && arr[mid] > arr[mid + 1])
+            return mid;
+        if (mid > low && arr[mid] < arr[mid - 1])
+            return (mid - 1);
+        if (arr[low] >= arr[mid])
+            return findPivot(arr, low, mid - 1);
+        return findPivot(arr, mid + 1, high);
+    }
+	 
 }
